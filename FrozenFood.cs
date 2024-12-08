9e7cab12fc62839ecab4e4368acb51e795973c86
Fuar11 <mcaa6556@gmail.com>
2024-12-07 20:29:38 -0500

    Added Broken Silence food items

--- FrozenFood.cs
+++ FrozenFood.cs
@@ -407,8 +407,8 @@ namespace FrozenFood
 
         public string GetFoodType(string item)
         {
-            if (item == "GEAR_Crackers" || item == "GEAR_KetchupChips" || item == "GEAR_BeefJerky" || item == "GEAR_GranolaBar" || item == "GEAR_CandyBar" || item.Contains("Stew") || item.Contains("Pie")) return "Dry";
-            else if (item.ToLowerInvariant().Contains("meat") || item.Contains("CohoSalmon") || item.Contains("LakeWhiteFish") || item.Contains("RainbowTrout") || item.Contains("SmallMouthBass") || item == "GEAR_PinnacleCanPeaches" || item == "GEAR_TomatoSoupCan" || item == "GEAR_CannedSardines" || item == "GEAR_PinnacleCanPeaches" || item == "GEAR_CondensedMilk" || item.Contains("Corn") || item.Contains("Broth")) return "Wet";
+            if (item == "GEAR_Crackers" || item.Contains("Cereal") || item == "GEAR_KetchupChips" || item == "GEAR_BeefJerky" || item == "GEAR_GranolaBar" || item == "GEAR_CandyBar" || item.Contains("Stew") || item.Contains("Pie") || item == "GEAR_CuredMeat" || item == "GEAR_CuredFish" || item == "GEAR_DriedApples" || item.ToLowerInvariant().Contains("pemmican")) return "Dry";
+            else if (item.ToLowerInvariant().Contains("meat") || item.Contains("CohoSalmon") || item.Contains("LakeWhiteFish") || item.Contains("RainbowTrout") || item.Contains("SmallMouthBass") || item == "GEAR_PinnacleCanPeaches" || item == "GEAR_TomatoSoupCan" || item == "GEAR_CannedSardines" || item == "GEAR_PinnacleCanPeaches" || item == "GEAR_CondensedMilk" || item.Contains("Corn") || item.Contains("Broth") || item.Contains("Soup") || item.Contains("Pickles")) return "Wet";
             else return "Mid";
         }
 
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 using MelonLoader;
 using System.Reflection;
 
-//This is a C# comment. Comments have no impact on compilation.
-
 [assembly: AssemblyTitle(BuildInfo.ModName)]
 [assembly: AssemblyCopyright($"Created by Fuar")]
 
@@ -17,9 +15,5 @@ internal static class BuildInfo
 {
 	internal const string ModName = "Frozen Food";
 	internal const string ModAuthor = "Fuar";
-	/// <summary>
-	/// Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
-	/// Mods typically use 3 numbers. For example: 1.2.1
-	/// </summary>
-	internal const string ModVersion = "1.4.0";
+	internal const string ModVersion = "1.5.0";
 }
